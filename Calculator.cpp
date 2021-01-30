3b5122c74a314cd010d07514448efbcb97dde323
~ Iòrdan ~ <ixxjordan@hotmail.co.uk>
2021-01-30 05:17:29 +0000

    Add project files.

---
+++ Calculator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Calculator Console Application\n\n";
+    std::cout << "Please Enter the operation to perform. Format: a+b | a-b | a*b | a/b \n";
+    return 0;
+}
+
